1a6ef33b9beb73d23ee5b1942ca8df02d9cfbf7b
Bo <bo.van.laarhoven@gmail.com>
2022-12-23 22:48:48 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -60,29 +60,3 @@ if supported == true then
         })
     end
 end
-
-if supported == false then
-    local gamez = ({
-        [6847090259] = "bulkedup.lua",
-        [155615604] = "prisonlife.lua",
-        [10243982775] = "redbox.lua",
-        [9498006165] = "tappingsimulator.lua",
-        [9872472334] = "evade.lua",
-        [10324346056] = "evade.lua",
-        [10324347967] = "evade.lua",
-        [10539706691] = "evade.lua",
-        [10662542523] = "evade.lua",
-        [10808838353] = "evade.lua",
-        [11353528705] = "evade.lua",
-        [11353532384] = "evade.lua",
-    })[game.PlaceId]
-    
-    if gamez then
-        loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Robobo2022/Hydra-Network-V3/main/Games/" .. gamez))()
-    else
-        game:GetService("StarterGui"):SetCore("SendNotification", {
-            Title = "Hydra Network",
-            Text = "Hydra Network does not support this game"
-        })
-    end
-end
